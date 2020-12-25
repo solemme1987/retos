9109a1bb14461ae961d83ede9a49070a4d5936c1
Ricardo Cortes <ricacor3@gmail.com>
2020-12-25 11:24:45 -0500

    Retoo 2 terminado y funcionando

--- Día 2 - Progress Steps/script.js
+++ Día 2 - Progress Steps/script.js
@@ -1,33 +1,29 @@
-const progress = document.getElementById('')
-const prev = document.getElementById('')
-const next = document.getElementById('')
-const circles = document.querySelectorAll('')
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
 
-let currentActive = 1
+let currentActive = 1;
 
 next.addEventListener('click', () => {
-    currentActive++
 
-    if(currentActive > circles.length) {
-
-    }
-
-    update()
-})
+    currentActive++;
+    prev.removeAttribute('disabled');
+    update();
+});
 
 prev.addEventListener('click', () => {
-    currentActive--
-
-    if(currentActive < 1) {
-
-    }
-
-    update()
+    currentActive--;
+    next.removeAttribute('disabled');
+    update();
 })
 
 function update() {
+    //  con esto agrego la clase active a cada circulo 
+    // le agrega borde azul a cada uno con esa clase
     circles.forEach((circle, idx) => {
-        if(idx < currentActive) {
+        console.log('Index', idx);
+        if (idx < currentActive) {
             circle.classList.add('active')
         } else {
             circle.classList.remove('active')
@@ -36,12 +32,13 @@ function update() {
 
     const actives = document.querySelectorAll('.active')
 
-    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+    // agregamos a la linea azul la clase lo agrega por porcentaje segun la cantidad activa
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
 
-    if(currentActive === 1) {
-
-    } else if(currentActive === circles.length) {
-
+    if (currentActive === 1) {
+        prev.setAttribute('disabled', '');
+    } else if (currentActive === circles.length) {
+        next.setAttribute('disabled', '');
     } else {
 
     }
