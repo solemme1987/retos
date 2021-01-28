978686e6171a71b157c82afa477c1ab08e5c0aa4
Ricardo Cortes <ricacor3@gmail.com>
2021-01-28 14:35:01 -0500

    Reto blur terminado correctamente

--- Día 5 - Blurry Loading/script.js
+++ Día 5 - Blurry Loading/script.js
@@ -1,21 +1,31 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
 
-let load = 0
 
-let int = setInterval(blurring, 30)
+let load = 0;
+
+let opacity=1;
+
+let filtro=30;
+
+let int = setInterval(blurring, 30);
+
 
 function blurring() {
-    
 
-    if(){
-       
+
+    if(load<100){
+  
+      load++;
+      opacity= opacity-0.01;//Agrega la nueva opacidad al texto del loading
+
+      loadText.innerText =` ${load}%`;// aplica la nueva opacidad al loading
+      loadText.style.opacity=opacity.toFixed(2);//aplica  opacidad al texto del loading
+         
+      filtro = filtro - 0.30;// me disminuye el blur de la image
+      bg.style.filter=(`blur(${filtro}px)`);// aplica el nuevo blur a la imagen cada vez que  le resta
+         
     }
 
-    loadText.innerText = 
-    loadText.style.opacity = 
-    bg.style.filter = 
-}
 
-   
- 
+}
